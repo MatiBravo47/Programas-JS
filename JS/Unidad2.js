0950fdabb38028f1cda08f3cc72652852aca5262
MatiBravo47 <Matibravoneron@gmail.com>
2022-06-09 11:46:11 -0300

    Agrego ejercicio 5 y 6

--- JS/Unidad2.js
+++ JS/Unidad2.js
@@ -10,7 +10,7 @@
 // let n=parseInt(prompt("Ingrese un numero entero:"))
 // repetirTexto(n)
 
-//2.Diseniar una funcion que tenga como parametros dos numeros,y que calcule el maximo
+/*//2.Diseniar una funcion que tenga como parametros dos numeros,y que calcule el maximo
 function valorMaximo(x,z){
     let max=0; 
     if (x>z)
@@ -22,4 +22,52 @@ function valorMaximo(x,z){
 //programa principal 
 let n=parseInt(prompt("Ingrese un numero entero:"))
 let n1=parseInt(prompt("Ingrese otro numero entero:"))
-document.write(valorMaximo(n,n1))
+document.write(valorMaximo(n,n1))*/
+
+/*Crear una funcion que se llame promedio3,que reciba
+como parametro tres valores y devuelta el proemdio de 
+los mismos*/
+/*function promedio3(n,n1,n2){
+    return((n+n1+n2)/3)
+}
+//Programa principal 
+let n=parseInt(prompt("Ingrese un numero entero:"))
+let n1=parseInt(prompt("Ingrese otro numero:"))
+let n2=parseInt(prompt("Ingrese otro numero:"))
+document.write("El promedio de"+n+","+n1+","+n2+" es:"+ promedio3(n,n1,n2))*/
+
+
+/*Crear una funcion que lea notas hasta que ingrese -1 y 
+devuelve el promedio de las notas leidas.(aunque no se 
+suele leer valores en una funcion)Una funcion que se llame 
+siguiente,que reciba como parametro un valor entero, y
+que devuelva el siguiente del numero ingresado como parametro
+*/
+
+
+/*5)Una funcion que se llame doble,que reciba como 
+parametro un valor, y devuelva el doble del numero ingresado
+como parametro*/
+/*function doble(n){
+    return (n*2)
+}
+//Programa principal
+n=parseInt(prompt("Ingrese un numero"))
+document.write("El doble de ese numero es: "+doble(n))*/
+
+/*6)Una funcion que se llame cuadrado,que reciba como parametro
+un visualViewport,y devuelva el valor del numero ingresado 
+como parametro elevado al cuadrado.*/
+/*function cuadrado(n){
+    return (n**2)
+}
+//Programa principal
+n=parseInt(prompt("Ingrese un numero"))
+document.write("El cuadrado de ese numero es: "+cuadrado(n))*/
+
+/*7)Una funcion que se denomine imprimirValores que dado un 
+numero por parametro, imprimi cual es el valor siguiente,
+el doble y el cuadrado. Usando las funciones definidas en 
+los puntos 5), 6) y 7):siguiente , el doble y el cuadrado*/
+
+
