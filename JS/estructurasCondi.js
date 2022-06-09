ab111464423ffb3825c48c268c47d9ae6a12418b
MatiBravo47 <Matibravoneron@gmail.com>
2022-06-08 21:22:56 -0300

    agrego un ejercicio

---
+++ JS/estructurasCondi.js
@@ -0,0 +1,19 @@
+/*Leer la hora por pantalla(Numero entero)y saludar en la
+consola segun el horario:Buenos dias(14 horas),buenas
+tardes (19 horas) o buenas noches*/
+
+/*let hora;
+hora=parseInt(prompt("Ingrese la hora:"))
+if (hora<=14){
+    console.log("Buenos dias")
+}else if (hora<=19){
+    console.log("Buenas tardes")
+}
+else{
+    console.log("Buenas noches")
+}*/
+
+/* Crear un programa que segun el numero de dia de la semana
+solicitado,diga "buen lunes""buen martes"",etc.*/
+
+
