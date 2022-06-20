716065fbfd9824066eee3439230579040a37752f
MatiBravo47 <Matibravoneron@gmail.com>
2022-06-20 18:51:59 -0300

    Agregue todos los pilotos

---
+++ JS/data.js
@@ -0,0 +1,95 @@
+var data=[
+    {Codigo:"cod1",
+    nombre:"Carlos Sainz",
+    equipo:"ferrari"
+    },
+    {Codigo:"cod2",
+    nombre:"Charles Lecrerc",
+    equipo:"ferrari"
+    },
+    {Codigo:"cod3",
+    nombre:"Max Verstappen",
+    equipo:"Red Bull"
+    },
+    {Codigo:"cod4",
+    nombre:"Checo Perez",
+    equipo:"Red Bull"
+    },
+    {Codigo:"cod5",
+    nombre:"Lando Norris",
+    equipo:"Mclaren"
+    },
+    {Codigo:"cod6",
+    nombre:"Daniel Ricciardo",
+    equipo:"Mclaren"
+    },
+    {Codigo:"cod7",
+    nombre:"Fernando Alonso",
+    equipo:"Alpine"
+    },
+    {Codigo:"cod8",
+    nombre:"Esteban Ocon",
+    equipo:"Alpine"
+    },
+    {Codigo:"cod9",
+    nombre:"Kevin Maggnussen",
+    equipo:"Haas"
+    },
+    {Codigo:"cod10",
+    nombre:"Mick Schumacher",
+    equipo:"Haas"
+    },
+    {Codigo:"cod11",
+    nombre:"Lewis Hamilton",
+    equipo:"Mercedes"
+    },
+    {Codigo:"cod12",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+    {Codigo:"",
+    nombre:"",
+    equipo:""
+    },
+
+]
