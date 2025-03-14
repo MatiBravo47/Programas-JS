d6d448a1a80071b2bb6bb0d837cde1ed90d8f5dc
Matias <Matibravoneron@gmail.com>
2025-03-14 16:50:28 -0300

    Mejoro claridad

--- JS/Unidad2.js
+++ JS/Unidad2.js
@@ -3,7 +3,6 @@
 
 // 1.Realizar una funcion,a la que se le pase como parametro un numero N,
 // y muestre por consola N veces,el siguiente mensaje: "Bienvenidos al curso de Full Stack"
-
 function repetirTexto(n){
     for (let i = 0; i < n; i++)
           console.log("Bienvenidos al curso full Stack")
@@ -13,7 +12,6 @@ let n = parseInt(prompt("Ingrese un numero entero:"))
 repetirTexto(n)
 
 //2.Diseniar una funcion que tenga como parametros dos numeros,y que calcule el maximo
-
 function valorMaximo(x,z){
     let max = 0; 
     if (x > z)
@@ -26,7 +24,6 @@ function valorMaximo(x,z){
 let n = parseInt(prompt("Ingrese un numero entero:"))
 let n1 = parseInt(prompt("Ingrese otro numero entero:"))
 document.write(valorMaximo(n, n1))
-
 
 /*
 3)Crear una funcion que se llame promedio3,que reciba
@@ -42,7 +39,6 @@ let n1 = parseInt(prompt("Ingrese otro numero:"))
 let n2 = parseInt(prompt("Ingrese otro numero:"))
 document.write("El promedio de" + n + "," + n1 + "," + n2 +" es:" + promedio3(n, n1, n2))
 
-
 /*
 4)Crear una funcion que lea notas hasta que ingrese -1 y 
 devuelve el promedio de las notas leidas.(aunque no se 
@@ -57,7 +53,6 @@ que devuelva el siguiente del numero ingresado como parametro
 parametro un valor, y devuelva el doble del numero ingresado
 como parametro
 */
-
 function doble(n){
     return (n * 2)
 }
@@ -78,14 +73,12 @@ function cuadrado(n){
 n = parseInt(prompt("Ingrese un numero"))
 document.write("El cuadrado de ese numero es: "+cuadrado(n))
 
-
 /*
 7)Una funcion que se denomine imprimirValores que dado un 
 numero por parametro, imprimi cual es el valor siguiente,
 el doble y el cuadrado. Usando las funciones definidas en 
 los puntos 5), 6) y 7):siguiente , el doble y el cuadrado
 */
-
 function cuadrado(n){
     return (n**2)
 }
@@ -104,14 +97,12 @@ function imprimirValores(n){
 n = parseInt(prompt("Ingrese un numero"))
 imprimirValores(n)
 
-
 /*
 8)Una funcion que se denomine imprimirELDobleDelSiguiente
 que dado un numero por parametro,imprima cual es el valor
 siguiente del doble.usando las funciones definidas de los 
 puntos 5),6): siguiente,el doble.
 */ 
-
 function siguiente(n){
     return(n + 1)
 }
@@ -125,14 +116,12 @@ function imprimirElDobleDelSiguiente(n){
 n = parseInt(prompt("Ingrese un numero"))
 imprimirElDobleDelSiguiente(n)
 
-
 /*
 9)Una funcion que se denomine imprimirElDobleDelSiguienteAlCuadrado
 que dado un numero por parametro,imprima cual es el valor 
 siguiente al del doble.Usando las funciones definidas 
 en los puntos 5),6) y 7): siguiente,el doble y el cuadrado.
 */
-
 function doble(n){
     return (n*2)
 }
@@ -175,7 +164,6 @@ function superficieCuadrado(n){
 n = parseInt(prompt("Ingrese longitud de un lado del cuadrado"))
 document.write("La superficie del cuadrado es:"+ superficieCuadrado(n))
 
-
 /*
 12)Una funcion que dado el radio de un circulo devuelva
 la circunferencia (perimetro del circulo)
@@ -188,12 +176,10 @@ function circunferencia(r){
 r = parseInt(prompt("Ingrese radio del circulo"))
 document.write("La circunferencia del circulo es"+ circunferencia(r))
 
-
 /*
 13)Una funcion que dado el radio de un circulo devuelva
 el area del circulo
 */
-
 const PI = 3.14;
 function areaCirculo(r){
     return(PI * (r**2))
@@ -202,13 +188,11 @@ function areaCirculo(r){
 r = parseInt(prompt("Ingrese radio del circulo"))
 document.write("El area del circulo es "+areaCirculo(r))
 
-
 /*
 14)Una funcion que dado un numero de mes y me devuelva la 
 cantidad de dias de ese mes(suponiendo que no es un 
 anio bisiesto)
 */
-
 mes = parseInt(prompt("Ingrese numero de mes"))
 switch(mes) {
     case 1:
@@ -232,12 +216,10 @@ switch(mes) {
 }
 document.write("Ese mes tiene "+ dias +" dias")
 
-
 /*
 15)Una funcion que reciba como parametro un anio 
 y diga si ese anio es bisiesto. 
 */
-
 function esBisiesto(anio){
 if ((anio % 4 == 0) && (anio % 100 != 0 || anio % 400 == 0 )){
     document.write("El anio es bisiesto");
@@ -260,7 +242,6 @@ estudiantes de su clase.Itere el array utilizando un
 bucle while y luego imprima todas las edades en la 
 consola.
 */
-
 const edades = [18, 24, 22, 24, 24, 27];
 let i = 0;
 let text = "";
@@ -270,13 +251,11 @@ while (edades[i]){
 }
 document.getElementById("ejemplo").innerHTML = text;
 
-
 /*
 20)Al ejercicio 1 agregue un condicional dentro del ciclo
 while para imprimir solo numeros pares.Cambie el ciclo 
 para usar un ciclo "for" en lugar de un ciclo "while".
 */
-
 const edades = [18, 24, 22, 24, 24, 27]
 let i = 0;
 let text = "";
@@ -287,25 +266,20 @@ for (;edades[i];) {
 } 
 document.getElementById("ejemplo").innerHTML = text;
 
-
 /*
 21)Escriba una funcion que reciba un array como parametro 
 e imprima el numero mas bajo para cualquier array que se 
 pase 
 */
-
 const edades1 =[18, 24, 22, 24, 24, 27]
 document.getElementById("ejemplo").innerHTML = "El minimo del Array es: "+ (Math.min.apply(null, edades1));
-
 
 /*
 22)Escriba una funcion que reciba un array como parametro
 e imprima el numero mas grande de la array en la consola.
 */
-
 const edades2 = [18, 24, 22, 24, 24, 27]
 console.log((Math.max.apply(null, edades2)));
-
 
 /*
 23)Escriba una funcion que reciba dos parametros, una array
@@ -314,7 +288,6 @@ posicion dada(basado en uno)en la consola.Por ejemplo,
 dada la siguiente array e indique,la funcion imprimira '6'
 let array=[3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100]
 let index =1;*/
-
 let array=[3, 6, 67, 6, 23, 11, 100, 8, 93, 0, 17, 24, 7, 1, 33, 45, 28, 33, 23, 12, 99, 100]
 num = parseInt(prompt("Ingrese un numero"))
 console.log(array.indexOf(num))
@@ -334,12 +307,10 @@ myColor=["Red","Green","White","Black"]
 Deberia ver lo siguiente en su consola: "Rojo","Verde","Blanco"
 "Negro"
 */ 
-
 myColor=["Red", "Green", "White", "Black"]
 for (let x of myColor) {
     console.log(x + " ,")
 }
-
 
 /*
 26)Escriba una funcion de JavaScript que invierta un numero.
@@ -349,7 +320,6 @@ Por ejemplo,si x = 32443,la salida deberia ser 34423.
 .reverse():volcar 
 .join(""):Volver a convertir en cadena.
 */
-
 function invertir(n){
     const invertido = n.toString().split("").reverse().join("");
     return invertido;
@@ -358,14 +328,12 @@ function invertir(n){
 num = parseInt(prompt("Ingrese un numero"))
 document.write(invertir(num))
 
-
 /*
 27)Escriba una funcion de JavaScript que devuelva una
 cadena en orden alfabetico.Por ejemplo,si x='webmaster',
 la salida deberia ser 'abeemrstw'.La puntuacion y los 
 numeros no se pasan en la cadena.
 */
-
 function Alfabetica(texto){
     const alfa = texto.split("").sort().join("");
     return alfa;
@@ -374,14 +342,11 @@ function Alfabetica(texto){
 texto = prompt("Ingrese una palabra")
 document.write(Alfabetica(texto))
 
-
-
 /*
 28)Escriba una funcion de JavaScript que convierta la 
 primera letra de cada palabra a mayusculas.Por ejemplo,si 
 x="prince of persia",la salida deberia ser "Prince of Persia"
 */
-
 function Mayusculas(texto){
     mayus = texto.charAt(0).toUpperCase() + texto.slice(1) ;
     return mayus;
@@ -390,13 +355,11 @@ function Mayusculas(texto){
 texto=prompt("Ingrese texto")
 document.write(Mayusculas(texto))
 
-
 /*
 29)Escriba una funcion de JavaScript que busque la palabra 
 mas larga de una frase.Por ejemplo,si x="tutorial de 
 desarrollo web" el resultado deberia ser "Desarrollo"
 */
-
 const palabraMasLarga = cadena => {
     //separa por espacios
     const separadaPorEspacios = cadena.split(" ");
--- JS/unidad1.js
+++ JS/unidad1.js
@@ -4,37 +4,27 @@
 //alert que diga “Hello World”.
 alert("Hello World")
 
-
 //2. Escribe un programa de una sola línea que escriba en la pantalla un texto que
 //diga “Hello World” (document.write).
-
 document.write("Hello World")
-
 
 //3. Escribe un programa de una sola línea que escriba en la pantalla el resultado
 //de sumar 3 + 5.
-
 document.write(3 + 5)
-
 
 //4. Escribe un programa de dos líneas que pida el nombre del usuario con un
 //prompt y escriba un texto que diga “Hola nombreUsuario”
-
-//let nombre = prompt ("Ingrese su nombre", "")
+let nombre = prompt ("Ingrese su nombre", "")
 document.write("Hola " + nombre)
-
 
 //5. Escribe un programa de tres líneas que pida un número, pida otro número y
 //escriba el resultado de sumar estos dos números.
-
 let num1 = prompt ('Ingrese un numero');
 let num2 = prompt ("Ingrese otro numero ");
 document.write("la suma es: " + (parseInt(num1) + parseInt(num2)))
 
-
 //6. Escribe un programa que pida dos números y escriba en la pantalla cual es el
 //mayor
-
 let num1 = prompt ('Ingrese un numero');
 let num2 = prompt ("Ingrese otro numero ");
 if (num1 > num2) {
@@ -42,11 +32,9 @@ if (num1 > num2) {
 }else{
  document.write ("el mayor es: " + num2) }
 
-
 //7. Escribe un programa que pida 3 números y escriba en la pantalla el mayor de
 //los tres 
 //                                                                                    NO ME SALIO!!!
-
 let n1 = prompt("Escribe un número");
 let n2 = prompt("Escribe otro número");
 let n3 = prompt("Escribe otro número");
@@ -58,9 +46,7 @@ document.write(n2);
 document.write(n3);
 }
 
-
 //8. Escribe un programa que pida un número y diga si es divisible por 2
-
 let n1 = prompt("Ingrese un numero");
 if (n1 % 2 === 0) {
     document.write("El numero "+ n1 + " es divisible ")
@@ -68,13 +54,11 @@ if (n1 % 2 === 0) {
     document.write("El numero "+n1 + " no es divisible")
 }
 
-
 //9. Escribe un programa que pida una frase y escriba cuantas veces aparece la
 //letra a
 
 
 //10. Escribe un programa que pida una frase y escriba las vocales que aparecen
-
  let text = prompt("Ingrese una frase ");
  let nText = text.length;
  let i;
@@ -86,7 +70,6 @@ if (n1 % 2 === 0) {
  }
  }
 
-
 // 11. Escribe un programa que pida una frase y escriba cuántas de las letras que
 // tiene son vocales
 
@@ -97,14 +80,12 @@ if (n1 % 2 === 0) {
 
 //13.Escribe un programa que pida un numero y nos diga si es divisible 
 //por 2,3,5 o 7(solo hay que comprobar si lo es por una de los cuatro)
-
  let n1 = prompt("Escribe un numero");
  if (n1 % 2 === 0 || n1 % 3 === 0 || n1 %5 === 0 || n1 % 7=== 0){
      document.write("Es divisible por 2,3,5 o 7");
  } else {
  document.write("No es divisible ni por 2,ni por 3,ni por 5 ni por 7 ")
  }
-
 
 //14.Aniadir al ejercicio anterior que nos diga por cual de los cuatro es divisible
 //(hay que decir todos por los que es divisible )
@@ -126,9 +107,7 @@ if (n1 % 2 === 0) {
      document.write("No es divisible ni por 2, ni por 3, ni por 5,ni por 7");
  }
 
-
 //15. Escribir un programa que escriba en pantalla los divisores de un numero dado
-
  let n1 = prompt ("Ingrese un numero")
  let i;
  for (i = 2; i <= (n1 / 2) ; i++){
@@ -139,7 +118,6 @@ if (n1 % 2 === 0) {
 
 
 //16.Escribir un programa que escriba en pantalla los divisores comunes de dos numeros dados
-
 let n1 = prompt ("Ingrese un numero");
 let n2 = prompt ("Ingrese un numero");
 let menor;
@@ -155,10 +133,8 @@ if (n1 % i ===0 && n2 % i === 0){
 }
 }
 
-
 //17.Escribir un programa que nos diga si un numero dado es primo(no es divisible 
 //por ninguno otro numero que no sea el mismo o la unidad )
-
  let n1 = prompt ("Ingrese un numero");
  let i;
  primo = true;
@@ -172,9 +148,7 @@ if (n1 % i ===0 && n2 % i === 0){
      document.write("No es primo")
  } 
 
-
 //18.Pide la edad y si es mayor de 18 anios indica que ya puede conducir 
-
  let edad = prompt("Ingrese edad")
  if (edad >= 18) {
      document.write("Puede conducir")
@@ -190,7 +164,6 @@ if (n1 % i ===0 && n2 % i === 0){
 //6-7: Bien
 //7-9: Notable
 //9-10: Sobresaliente
-
  let nota = prompt("Ingrese la nota");
  //Si es un numero
  if (Number(nota) == nota){
@@ -218,10 +191,8 @@ if (n1 % i ===0 && n2 % i === 0){
   alert("Nota erronea")
  }
 
-
 //20.Realiza un script que pida cadenas de texto hasta que se pulse "cancelar"
 //Al salir con "cancelar" deben mostrarse todas las cadenas concatenadas con un guion
-
  let resultado =""
  do{
  let cadena= prompt("Ingrese cadena de texto ");
@@ -233,11 +204,9 @@ if (n1 % i ===0 && n2 % i === 0){
  }while (confirm("Desea continuar"));
  document.write(resultado);
 
-
 /*21.Realiza un script que pida numeros hasta que se pulse "cancelar".
 Si no es un numero debera indicarse con un "alert" y seguir pidiendo.
 Al salir con "cancelar" debera indicarse la suma total de los numeros introducidos*/
-
  Resultado=0;
 do{
     let num = prompt("Ingrese un numero")
@@ -248,8 +217,6 @@ do{
 } 
 while (confirm("Desea continuar"));
 document.write(Resultado)
-
-
 
 /*
 22)Realizar una pagina con un script que calcule el valor
@@ -265,7 +232,6 @@ alert y volver a preguntar.
 *Debera de repetirse el proceso hasta que el usuario pulse
 "calcelar"
 */
-
 DNI = (prompt("Ingrese numero de dni"))  
 resto = DNI % 23
 switch (resto) {
@@ -340,8 +306,6 @@ switch (resto) {
         break;
 }
 
-
-
 /*
 23)Realiza un script que escriba una piramide del 1 al 30
 de la siguiente forma 
@@ -352,7 +316,6 @@ de la siguiente forma
 55555
 666666
 */
-
 let i,rep;
 for (i = 1; i <= 30; i++) { //numero cada linea 
     for (rep = 0; rep < i; rep++){//Repeticiones linea
@@ -360,7 +323,6 @@ for (i = 1; i <= 30; i++) { //numero cada linea
     }
     document.write("<br>") //Baja linea
 }
-
 
 /*
 24)Haz un script que escriba una piramide inversa de los 
@@ -373,8 +335,6 @@ forma:(Suponiendo que indica 6)
 22
 1
 */
-
-
 let i,rep;
 let num = parseInt(prompt("Ingrese un numero"))//6
 for (i = num; i >= 1; i--){ //Numero empieza en 6 y decrece 
@@ -383,8 +343,6 @@ for (i = num; i >= 1; i--){ //Numero empieza en 6 y decrece
     }
     document.write("<br>")
 }
-
-
 
 /*
 25)Un script que escriba los numeros del 1 al 500,que 
@@ -401,8 +359,6 @@ muestre una linea horizontal.Por ejemplo:
 9(multiplo de 9)
 10
 */
-
-
 for (i = 1; i <= 500; i++){
     document.write(i)
     if (i % 4 == 0) {
@@ -416,8 +372,3 @@ for (i = 1; i <= 500; i++){
     }
     document.write("<br>")    
 }
-
-
-
-
-
